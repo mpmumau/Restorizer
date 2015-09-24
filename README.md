28aa7f981dd3531ec1dafbe56812dc48d7084add
Matt Mumau <mpmumau@mumauwebmarketing.com>
2015-09-24 14:17:04 -0400

    Documentation fixes, gitignore

--- README.md
+++ README.md
@@ -4,8 +4,8 @@ A PHP-based RESTful web service with psuedo-MVC properties.
 
 Copyright (c) 2015 Matt Mumau
 
-Licensed under The MIT License
-For full copyright and license information, please see the LICENSE.txt
+Licensed under The MIT License. 
+For full copyright and license information, please see the LICENSE.txt.
 Redistributions of files must retain the above copyright notice.
 
 ## Changelog
--- lib/Database.php
+++ lib/Database.php
@@ -1,4 +1,16 @@
 <?php
+/**
+ * Restorizer: A PHP-based RESTful web service with psuedo-MVC properties.
+ * Copyright (c) Matt Mumau
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Matt Mumau
+ * @since         0.1.0
+ * @license       https://opensource.org/licenses/MIT MIT License
+ */
 
 include_once("DatabaseConfig.php");
 
